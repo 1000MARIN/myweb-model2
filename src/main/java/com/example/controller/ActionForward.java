209a751864650c107f3a65195c5daeeda98e882a
1000marin <leexjin@gmail.com>
2021-10-05 10:55:32 +0900

    Create ActionForward.java

---
+++ src/main/java/com/example/controller/ActionForward.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import lombok.Data;
+
+@Data
+public class ActionForward {
+	
+	private boolean isRedirect = false;		// false면 forward, true
+	private String path = null;				// 리다이렉트일 경우 path는 *.do 형식의 경로
+											// 포워드 일 경우 path는 .jsp 형식의 경로
+	
+}
