6324cc1058b758c3639b4cd11bc1d76a729e2101
1000marin <leexjin@gmail.com>
2021-10-05 10:55:34 +0900

    Update MyDispatcherServlet.java

--- src/main/java/com/example/controller/MyDispatcherServlet.java
+++ src/main/java/com/example/controller/MyDispatcherServlet.java
@@ -2,13 +2,12 @@ package com.example.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 // MVC(모델2) 			: Moedl-View-Controller 패턴으로 작성하는 방식
 
@@ -41,17 +40,46 @@ public class MyDispatcherServlet extends HttpServlet {
 		String command = requestURI.substring(contextPath.length() + 1);	
 		System.out.println("command : " + command);						// "/index.do"
 		
+		ActionForward forward = null;
 		
 		// 2. 요청 명령어에 해당하는 작업 처리하기
-		
+		if (command.equals("/index.do")) {
+			
+			// 처리..
+			
+			// forwarding : 서버 실행흐름 내에서 jsp를 바로 실행하는 방식
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("index");
+			
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			dispatcher.forward(request, response);
+			
+		} else if (command.equals("/memberJoin.do")) {
+			
+		}
 		
 		
 		// 3. 작업 처리 후 사용자에게 알맞은 응답(화면 또는 데이터)을 보내기
-		
-		
+		// 이동방식 2가지: redirect (*.do) , forward (.jsp) 
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath()); // *.do 리다이렉트로 다시 요청 받음
+				
+			} else { // forward 이동
+				String prefix = "/WEB-INF/views/";
+				String suffix = ".jsp";
+				String path = prefix + forward.getPath() + suffix;
+				
+				RequestDispatcher dispatcher = request.getRequestDispatcher(path);	// .jsp 바로 실행
+				dispatcher.forward(request, response);
+				
+				
+			}
+		}
 		
 		
 	} // end of service
 	
-
-}
+} // end of MyDispatcherServlet
