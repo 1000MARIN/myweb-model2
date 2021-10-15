23f1ef658d15569a55b96204eedb24001c02c3ec
1000marin <leexjin@gmail.com>
2021-10-16 08:47:50 +0900

    Update MemberLoginAction.java

--- src/main/java/com/example/controller/member/MemberLoginAction.java
+++ src/main/java/com/example/controller/member/MemberLoginAction.java
@@ -1,7 +1,10 @@
 package com.example.controller.member;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletSecurityElement;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.example.controller.Action;
 import com.example.controller.ActionForward;
@@ -10,7 +13,16 @@ public class MemberLoginAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+		
+		// 사용자 세션 구하기
+		HttpSession session = request.getSession();
+		
+		// 애플리케이션 단위 영역객체 구하기
+		ServletContext application = request.getServletContext();
+		
+		
+		
+		
 		ActionForward forward = new ActionForward();
 		forward.setRedirect(false);
 		forward.setPath("member/login");
